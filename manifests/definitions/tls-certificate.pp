6163121b67e7c7b0e364a683cf23bab4cb5241c3
John Florian <jflorian@doubledog.org>
2010-04-18 18:15:12 -0400

    New - dovecot and local smtp-auth
    This represents another major step towards configuring astro as our new mail
    server.  Dovecot is now installed and configured to service IMAP connections
    and also to provide a local socket for Postfix to utilize for SMTP-AUTH.  The
    OpenSSL module now allows certificate installation w/o a corresponding key,
    such as is required with a CA certificate.
    
    - install and configure Dovecot
    - configure Postfix to use Dovecot's socket for SMTP-AUTH
    - install doubledog-cacert certificate
    - install doubledog-astro-imap certificate
    - install doubledog-astro-smtp certificate
    - fix postfix class to use "modules" in file source reference

--- manifests/definitions/tls-certificate.pp
+++ manifests/definitions/tls-certificate.pp
@@ -15,7 +15,7 @@ Example usage:
 
     node "foo.example.com" {
 
-        include openssl
+    include openssl
 
     openssl::tls-certificate { "domain-smtp":
         key_source      => "puppet:///private/domain-smtp.key",
@@ -24,16 +24,18 @@ Example usage:
 
 */
 
-define openssl::tls-certificate ($ensure="present", $key_source, $cert_source) {
+define openssl::tls-certificate ($ensure="present", $key_source="", $cert_source) {
 
-    file {"/etc/pki/tls/private/${name}.key":
-        ensure  => $ensure,
-        mode    => "0600",
-        require => Package["openssl"],
-        selrole => "object_r",
-        seltype => "cert_t",
-        seluser => "system_u",
-        source  => $key_source,
+    if $key_source != "" {
+        file {"/etc/pki/tls/private/${name}.key":
+            ensure  => $ensure,
+            mode    => "0600",
+            require => Package["openssl"],
+            selrole => "object_r",
+            seltype => "cert_t",
+            seluser => "system_u",
+            source  => $key_source,
+        }
     }
 
     file {"/etc/pki/tls/certs/${name}.crt":
