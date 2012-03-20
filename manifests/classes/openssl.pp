c5aa1ed1384a541575db1156f4ebafc3298e0f4f
John Florian <jflorian@doubledog.org>
2012-03-20 19:20:55 -0400

    Refactor - move openssl.cnf to private-domain
    The file provided with the module is now the default from Fedora 16,
    plus a simple notification header stating as such.

--- manifests/classes/openssl.pp
+++ manifests/classes/openssl.pp
@@ -2,17 +2,21 @@
 
 class openssl {
 
-    package { "openssl":
+    package { 'openssl':
 	ensure	=> installed,
     }
 
-    file { "/etc/pki/tls/openssl.cnf":
-        group	=> "root",
+    file { '/etc/pki/tls/openssl.cnf':
+        group	=> 'root',
         # Must be world readable; git reads this, for example.
-        mode    => "0644",
-        owner   => "root",
-        require => Package["openssl"],
-        source  => "puppet:///modules/openssl/openssl.cnf",
+        mode    => '0644',
+        owner   => 'root',
+        require => Package['openssl'],
+        source  => [
+            'puppet:///private-host/openssl/openssl.cnf',
+            'puppet:///private-domain/openssl/openssl.cnf',
+            'puppet:///modules/openssl/openssl.cnf',
+        ],
     }
 
 }
