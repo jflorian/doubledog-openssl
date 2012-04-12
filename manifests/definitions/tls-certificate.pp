5f3c42507e04e632e6c93d3c50ad47dfdae29e46
John Florian <jflorian@doubledog.org>
2012-04-12 15:02:34 -0400

    Janitorial - style conformance

--- manifests/definitions/tls-certificate.pp
+++ manifests/definitions/tls-certificate.pp
@@ -1,55 +1,56 @@
-/*
-== Definition: openssl::tls-certificate
+# modules/openssl/manifests/definitions/tls-certificate.pp
+#
+# Synopsis:
+#       Installs a TLS certificate (key and certificate files) for openssl.
+#
+# Parameters:
+#       Name__________  Default_______  Description___________________________
+#
+#       name                            instance name
+#       ensure          present         instance is to be present/absent
+#       key_source      ''              URI of private certificate key file
+#       cert_source                     URI of public certificate file
+#
+# Requires:
+#       Class['openssl']
+#
+# Example usage:
+#
+#       include openssl
+#
+#       openssl::tls-certificate { 'domain-smtp':
+#           key_source      => 'puppet:///private/domain-smtp.key',
+#           cert_source     => 'puppet:///private/domain-smtp.crt',
+#           notify          => Service['smtpd'],
+#       }
 
-Installs a TLS certificate (comprised of a key file and a certificate file).
 
-Parameters:
-- *name*: the name of the certificate
-- *key_source*: resource URI where the private key can be obtained
-- *cert_source*: resource URI where the public certificate can be obtained
+define openssl::tls-certificate ($ensure='present', $key_source='', $cert_source) {
 
-Requires:
-- Class["openssl"]
-
-Example usage:
-
-    include openssl
-
-    openssl::tls-certificate { "domain-smtp":
-        key_source      => "puppet:///private/domain-smtp.key",
-        cert_source     => "puppet:///private/domain-smtp.crt",
-        notify          => Service["httpd"],
-    }
-
-*/
-
-define openssl::tls-certificate ($ensure="present", $key_source="", $cert_source) {
-
-    if $key_source != "" {
-        file {"/etc/pki/tls/private/${name}.key":
+    if $key_source != '' {
+        file { "/etc/pki/tls/private/${name}.key":
             ensure      => $ensure,
-            group       => "root",
-            mode        => "0600",
-            owner       => "root",
-            require     => Package["openssl"],
-            selrole     => "object_r",
-            seltype     => "cert_t",
-            seluser     => "system_u",
+            group       => 'root',
+            mode        => '0600',
+            owner       => 'root',
+            require     => Package['openssl'],
+            selrole     => 'object_r',
+            seltype     => 'cert_t',
+            seluser     => 'system_u',
             source      => $key_source,
         }
     }
 
-    file {"/etc/pki/tls/certs/${name}.crt":
+    file { "/etc/pki/tls/certs/${name}.crt":
         ensure          => $ensure,
-        group           => "root",
-        mode            => "0644",
-        owner           => "root",
-        require         => Package["openssl"],
-        selrole         => "object_r",
-        seltype         => "cert_t",
-        seluser         => "system_u",
+        group           => 'root',
+        mode            => '0644',
+        owner           => 'root',
+        require         => Package['openssl'],
+        selrole         => 'object_r',
+        seltype         => 'cert_t',
+        seluser         => 'system_u',
         source          => $cert_source,
     }
 
 }
-
