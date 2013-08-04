1352c23dae75704596ad8e62c6899c3fe38925fe
John Florian <jflorian@doubledog.org>
2013-08-03 21:18:30 -0400

    Drop - openssl.cnf management
    This essentially did nothing but set defaults to make certificate
    creation via the openssl CLI a bit easier.  However, that same CLI sucks
    when it comes to handling v3 extensions like SubjectAltNames as a custom
    config is needed to do those.  I was not able to make work any of the
    tricks to obtain values via shell variables, which was hackish at best.
    A much better approach is to simply use an awesome tool like xca.  With
    that, there's no point in waiting for puppet to manage this for no gain.

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,19 +15,6 @@ class openssl {
         ensure  => installed,
     }
 
-    file { '/etc/pki/tls/openssl.cnf':
-        owner   => 'root',
-        group   => 'root',
-        # Must be world readable; git reads this, for example.
-        mode    => '0644',
-        require => Package['openssl'],
-        source  => [
-            'puppet:///private-host/openssl/openssl.cnf',
-            'puppet:///private-domain/openssl/openssl.cnf',
-            'puppet:///modules/openssl/openssl.cnf',
-        ],
-    }
-
     # This is needed by modules/openssl/manifests/tls-ca-certificate.pp, but
     # appears here to prevent duplicate declarations.  Plus it only need be
     # run once for all CA certificates installed.
